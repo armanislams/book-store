ed6b7e467717022d0fe8db3030ed72bd3285d24d
ArmanIslamTC <armanislam.ais@gmail.com>
2026-01-17 22:18:34 +0800

    added private navlinks

--- src/components/Common/NavLinks.jsx
+++ src/components/Common/NavLinks.jsx
@@ -1,7 +1,14 @@
+'use client'
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import React from "react";
 
 export default function NavLinks() {
+    const { data } = useSession();
+    const isLoggedIn = Boolean(data?.user)
+    console.log(isLoggedIn);
+    
+    
   return (
     <>
       <li>
@@ -14,11 +21,16 @@ export default function NavLinks() {
           Blogs
         </Link>
       </li>
-      <li>
-        <Link href={"/dashboard"} className="hover:bg-primary hover:text-white">
-          Dashboard
-        </Link>
-      </li>
+      {isLoggedIn && (
+        <li>
+          <Link
+            href={"/dashboard"}
+            className="hover:bg-primary hover:text-white"
+          >
+            Dashboard
+          </Link>
+        </li>
+      )}
     </>
   );
 }
--- src/proxy.js
+++ src/proxy.js
@@ -4,7 +4,6 @@ import { NextResponse } from "next/server";
 // This function can be marked `async` if using `await` inside
 export async function proxy(req) {
     const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET})
-    console.log('token', token);
     if (!token) {
         
         return NextResponse.redirect(new URL("/login", req.url));
